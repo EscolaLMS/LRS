b675f8d7cda24516c275617a1a04b8ca0d0f38b2
Tomasz Smolarek <tomasz.smolarek@escolasoft.com>
2022-04-06 15:27:08 +0200

    Additional params in cmi5 launch data

--- src/Services/Contracts/LrsServiceContract.php
+++ src/Services/Contracts/LrsServiceContract.php
@@ -4,5 +4,5 @@ namespace EscolaLms\Lrs\Services\Contracts;
 
 interface LrsServiceContract
 {
-    public function launchParams(int $courseId, string $token): array;
+    public function launchParams(string $token, ?int $courseId = null, ?int $topicId = null): array;
 }
