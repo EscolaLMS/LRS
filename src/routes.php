cb623a86158ee5f342f1f77d16babdc946990314
Hubert Krzysztofiak <hubert.krzysztofiak@escolasoft.com>
2022-05-05 14:36:55 +0200

    Add route from Core and remove trax from require

--- src/routes.php
+++ src/routes.php
@@ -1,22 +1,22 @@
 <?php
 
 use EscolaLms\Lrs\Http\Controllers\StatementController;
-use Illuminate\Support\Facades\Route;
+use EscolaLms\Core\Http\Facades\Route;
 use EscolaLms\Lrs\Http\Controllers\LrsController;
 
 Route::group(['prefix' => 'api'], function () {
     Route::group(['prefix' => '/cmi5'], function () {
         Route::post('/fetch', [LrsController::class, 'fetch'])->name("cmi5.fetch");
-        Route::group(['middleware' => ['auth:api']], function () {
+        Route::group(['middleware' => Route::apply(['auth:api'])], function () {
             Route::get('/courses/{id}', [LrsController::class, 'launchParams']);
         });
     });
 
-    Route::group(['prefix' => '/admin/cmi5', 'middleware' => ['auth:api']], function () {
+    Route::group(['prefix' => '/admin/cmi5', 'middleware' => Route::apply(['auth:api'])], function () {
         Route::get('/statements', [StatementController::class, 'statements']);
     });
 });
 
-Route::group(['middleware' => ['auth:token']], function () {
+Route::group(['middleware' => Route::apply(['auth:token'])], function () {
     Route::view('/lrs/{any?}', 'trax-front-lrs::app')->where('any', '.*')->middleware('web');
 });
